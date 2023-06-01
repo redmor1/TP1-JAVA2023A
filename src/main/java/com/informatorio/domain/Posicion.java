7af95f30893d65e6fc265927757bdab6112c0291
redmor1 <lightz17@hotmail.com>
2023-05-31 21:54:19 -0300

    Crear entidad Posicion

---
+++ src/main/java/com/informatorio/domain/Posicion.java
@@ -0,0 +1,24 @@
+package com.informatorio.domain;
+
+public class Posicion {
+    private String nombre;
+
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Posicion(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Posicion() {
+
+    }
+}
