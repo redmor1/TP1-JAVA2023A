172e25229a14755f82ee51e682c686f2eeb6be59
redmor1 <lightz17@hotmail.com>
2023-05-31 21:54:19 -0300

    Crear proyecto en Maven

---
+++ src/main/java/com/informatorio/App.java
@@ -0,0 +1,13 @@
+package com.informatorio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
